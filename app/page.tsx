cb32f6875b1b56118da3ec881788f74299e78d1a
krishnatayal1410 <krishnatayal1410@gmail.com>
2026-09-14 21:21:27 +0530

    feat: add complete footer to immersive homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { SiteNav } from "@/components/SiteNav";
+import { SiteFooter } from "@/components/SiteFooter";
 import { ImmersiveHome } from "@/components/ImmersiveHome";
 
 export default function HomePage() {
-  return <><SiteNav/><ImmersiveHome/></>;
+  return <><SiteNav/><ImmersiveHome/><SiteFooter/></>;
 }
